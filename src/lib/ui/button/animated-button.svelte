755d437d21bb7c50d0e41fc52fa7b75e4f65164d
Ryan Abraham <ryan.ryanabraham@gmail.com>
2025-06-10 10:42:26 -0700

    intial

---
+++ src/lib/ui/button/animated-button.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { cn } from "../../utils";
+  import type { ButtonProps } from "./button.svelte";
+
+  let {
+    class: _class,
+    variant = "default",
+    size = "default",
+    ref = $bindable(null),
+    href = undefined,
+    type = "button",
+    children,
+    ...restProps
+  }: ButtonProps = $props();
+</script>
+
+{#if href}
+  <a
+    {href}
+    class={cn(
+      "group relative grid overflow-hidden rounded-md px-4 py-2 dark:shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200",
+      _class
+    )}
+  >
+    <span>
+      <span
+        class="spark mask-gradient animate-flip before:animate-kitrotate absolute inset-0 h-[100%] w-[100%] overflow-hidden rounded-md [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] dark:before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)]
+        before:bg-[conic-gradient(from_0deg,transparent_0_330deg,blue_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]"
+        >,</span
+      >
+    </span>
+    <span
+      class="backdrop absolute inset-[0.9px] rounded-md bg-secondary dark:bg-neutral-900 transition-colors duration-200 group-hover:bg-neutral-200 dark:group-hover:bg-neutral-900"
+    ></span>
+    <span class="z-10 text-primary text-sm font-medium text-center">
+      {@render children?.()}
+    </span>
+  </a>
+{:else}
+  <button
+    class={cn(
+      "group relative grid overflow-hidden rounded-md px-4 py-2 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200",
+      _class
+    )}
+  >
+    <span>
+      <span
+        class="spark mask-gradient animate-flip before:animate-kitrotate absolute inset-0 h-[100%] w-[100%] overflow-hidden rounded-md [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]"
+      ></span>
+    </span>
+    <span
+      class="backdrop absolute inset-px rounded-[11px] bg-neutral-950 transition-colors duration-200 group-hover:bg-neutral-900"
+    ></span>
+    <span class="z-10 text-neutral-400 text-sm font-medium">
+      {@render children?.()}
+    </span>
+  </button>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import HeroSix from "\$lib/blocks/hero-six.svelte";
+	import { toggleMode,mode } from "mode-watcher";
+	import Button  from '$lib/ui/button/button.svelte';
+    import ContentOne from "$lib/blocks/content-one.svelte";
+    import TestimonialTwo from "$lib/blocks/testimonial-two.svelte";
+</script>
+
+<div>
+	<HeroSix/>
+	<ContentOne/>
+	<TestimonialTwo/>
+</div>
