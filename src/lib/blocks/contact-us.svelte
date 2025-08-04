3fecdebc5e7fea2eb0ae8057e842f334db711581
Ryan <62819056+chaoticthegreat@users.noreply.github.com>
2025-08-04 14:24:41 -0700

    new contact button

--- src/lib/blocks/contact-us.svelte
+++ src/lib/blocks/contact-us.svelte
@@ -9,25 +9,13 @@
       <h2 class="text-balance text-4xl font-semibold lg:text-5xl">
         Contact Us
       </h2>
-      <p class="mt-4">Have questions or want to partner with us?</p>
+      <p class="mt-4 text-xl py-4">Have questions or want to partner with us?</p>
 
       <form action="" class="mx-auto mt-10 max-w-sm lg:mt-12">
-        <div
-          class="bg-background has-[input:focus]:ring-muted relative grid grid-cols-[1fr_auto] items-center rounded-[calc(var(--radius)+0.75rem)] border pr-3 shadow shadow-zinc-950/5 has-[input:focus]:ring-2"
-        >
-          <Mail
-            class="text-caption pointer-events-none absolute inset-y-0 left-5 my-auto size-5"
-          />
-
-          <input
-            placeholder="Your mail address"
-            class="h-14 w-full bg-transparent pl-12 focus:outline-none"
-            type="email"
-          />
 
           <div class="md:pr-1.5 lg:pr-0">
-            <Button aria-label="submit" class="rounded-(--radius)" href="mailto:hannahli866@gmail.com">
-              <span class="hidden md:block">Get Started</span>
+            <Button  aria-label="submit" class="rounded-(--radius) py-6 px-10" href="mailto:hannahli866@gmail.com">
+              <span class="hidden md:block text-lg">Fill out this form</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -45,7 +33,6 @@
               >
             </Button>
           </div>
-        </div>
       </form>
     </div>
   </div>
