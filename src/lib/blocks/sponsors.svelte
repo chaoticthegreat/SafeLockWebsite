99a1c2ae7dde2c52cdd458321a3e152984237438
Ryan <62819056+chaoticthegreat@users.noreply.github.com>
2025-08-04 13:40:32 -0700

    better sponsor scaling

--- src/lib/blocks/sponsors.svelte
+++ src/lib/blocks/sponsors.svelte
@@ -10,14 +10,14 @@
                 class="mx-auto mt-20 flex max-w-4xl flex-wrap items-center justify-center gap-x-12 gap-y-8 sm:gap-x-16 sm:gap-y-12"
         >
             <img
-                    class="h-20 w-fit rounded-2xl hover:scale-125"
+                    class="h-30 w-fit rounded-2xl hover:scale-125"
                     src="https://blueoceancompetition.org/wp-content/uploads/2024/05/Blue-Ocean-Competition-Logo.png"
                     alt="Blue Ocean Logo"
                     height="20"
                     width="auto"
             />
             <img
-                    class="h-20 w-fit rounded-2xl hover:scale-125"
+                    class="h-30 w-fit rounded-2xl hover:scale-125"
                     src="https://assets.biola.edu/4396738754672012438/attachment/5fa43caf97f6410001fb2a8a/preferred-logo-horizontal.jpg"
                     alt="Biola University Logo"
                     height="20"
